bde1fab2f0ccae0ab1db5c9870c9da6e228c3d3c
ioana <ioanaardelean555@gmail.com>
2024-02-05 22:15:24 +0200

    make gallery filter

--- js/custom.js
+++ js/custom.js
@@ -35,3 +35,18 @@ $('#carouselExampleIndicators').on('slid.bs.carousel', function (event) {
         })
     }
 });
+
+
+function filterImages(category) {
+
+
+    const images = document.querySelectorAll('.gallery__image');
+
+    images.forEach((image) => {
+      if (category === 'all' || image.classList.contains(category)) {
+        image.style.display = 'block';
+      } else {
+        image.style.display = 'none';
+      }
+    });
+  }
